9c953868ed5847f8547925b20ec396ae5480eede
Leonardo WIjaya <leonardo.wijaya003@binus.ac.id>
2021-02-28 22:00:57 +0700

    Creating Custom Authentication
    From YouTube

---
+++ login.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="row" style="margin-top:45px">
+            <div class="col-md-4 col-md-offset-4">
+                <h4>Login</h4><hr>
+                <form action="{{ route('auth.check') }}" method="POST">
+                @if(Session::get('fail'))
+                    <div class="alert alert-danger">
+                        {{ Session::get('fail') }}
+                    </div>
+                @endif
+
+                @csrf
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" class="form-control" name="email" placeholder="Enter your email adress" value="{{ old('email') }}">
+                        <span class="text-danger">@error('email'){{ $message }} @enderror</span>
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" name="password" placeholder="Enter your password">
+                        <span class="text-danger">@error('password'){{ $message }} @enderror</span>
+                    </div>
+                    <button type="submit" class="btn btn-block btn-primary">Login</button>
+                    <br>
+                    <a href="{{ route('auth.register') }}">Don't have an acoount? Register here!</a>
+                </form>
+                </div>
+        </div>
+    </div>
+
+</body>
+</html>
